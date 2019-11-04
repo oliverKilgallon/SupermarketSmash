4a8e611dc3ec57577134d7afd2ea0ac53e5a9c3d
A.Liggins U1760724 <U1760724@unimail.hud.ac.uk>
2019-11-04 16:06:48 +0000

    Movement and items ver1

---
+++ Assets/Alex/ItemScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    public int product;
+    public bool inWall;
+   
+    // Use this for initialization
+    void Start()
+    {
+        inWall = false;
+        product = Random.Range(0, 15);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            inWall = true;
+        }
+
+    }
+    /*void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            inWall = true;
+        }
+    }*/
+}
