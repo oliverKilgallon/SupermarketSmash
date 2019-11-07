eb88792e8532771a5f49c7876ed93b330619b1fe
u1654523 <u1654523@unimail.hud.ac.uk>
2019-11-07 14:23:59 +0000

    Added in camera object compensation to shift camera if an object is in the way to the left, right, or behind the camera. Updated player prefab to use a second mount for the camera as part of this compensation

--- Assets/Scripts/Camera/CameraCompensation.cs
+++ Assets/Scripts/Camera/CameraCompensation.cs
@@ -6,19 +6,44 @@ public class CameraCompensation : MonoBehaviour
     public Transform cameraObj;
 
     public float cameraRayDistance = 5.0f;
+    public float leftRightRayDistance = 1.0f;
+
+    private float compensationDist;
 
     /**Fire raycast behind tracked object, if hit is less than wanted distance,
      * translate object along object's forward vector until this is no longer true
      */ 
     void FixedUpdate()
     {
+        //Backwards object tracking
         if (Physics.Raycast(cameraObj.position, -1 * cameraObj.forward, out RaycastHit hit, cameraRayDistance))
         {
             if (hit.distance < cameraRayDistance)
             {
-                float compensationDist = cameraRayDistance - hit.distance;
+                compensationDist = cameraRayDistance - hit.distance;
                 cameraObj.Translate(transform.forward * compensationDist * Time.fixedDeltaTime, Space.Self);
             }
         }
+        
+        //Right side object tracking
+        if (Physics.Raycast(cameraObj.position, cameraObj.right, out RaycastHit rightHit, leftRightRayDistance / 2.0f))
+        {
+            if ((rightHit.distance / 2.0f) < leftRightRayDistance)
+            {
+                compensationDist = leftRightRayDistance - rightHit.distance;
+                cameraObj.Translate((transform.right) * compensationDist * Time.fixedDeltaTime, Space.Self);
+            }
+        }
+
+        //Left side object tracking
+        if (Physics.Raycast(cameraObj.position, -1 * cameraObj.right, out RaycastHit leftHit, cameraRayDistance / 2.0f))
+        {
+            if ((leftHit.distance / 2.0f) < cameraRayDistance)
+            {
+                compensationDist = cameraRayDistance - leftHit.distance;
+                cameraObj.Translate(transform.right * compensationDist * Time.fixedDeltaTime, Space.Self);
+            }
+        }
+        
     }
 }
