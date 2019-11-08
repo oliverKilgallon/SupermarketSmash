9b5dfc9466fc95d9114041f53d9ab6d657ad21d3
u1654523 <u1654523@unimail.hud.ac.uk>
2019-11-08 15:30:23 +0000

    Updated implementation of Wheel rotation script, still not final. Update player prefab as part of testing

---
+++ Assets/Scripts/TrolleyWheelRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrolleyWheelRotation : MonoBehaviour
+{
+    public Transform[] wheelTransforms;
+    public Rigidbody trolleyRigidbody;
+    
+    private Vector3[] lastWheelPositions;
+    private Vector3[] smoothedPositions;
+    private Vector3[] wheelVelocities;
+
+    private void Start()
+    {
+        lastWheelPositions = new Vector3[wheelTransforms.Length];
+        smoothedPositions = new Vector3[wheelTransforms.Length];
+        wheelVelocities = new Vector3[wheelTransforms.Length];
+
+        for (int i = 0; i < wheelTransforms.Length; i++)
+        {
+            lastWheelPositions[i] = wheelTransforms[i].position;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        for(int i = 0; i < wheelTransforms.Length; i++)
+        {
+            if (trolleyRigidbody.velocity.magnitude > 0)
+            {
+                smoothedPositions[i] = Vector3.SmoothDamp(wheelTransforms[i].position, lastWheelPositions[i], ref wheelVelocities[i], 0.3f);
+                
+                wheelTransforms[i].LookAt(smoothedPositions[i], Vector3.up);
+            }
+
+            lastWheelPositions[i] = smoothedPositions[i];
+        }
+    }
+}
