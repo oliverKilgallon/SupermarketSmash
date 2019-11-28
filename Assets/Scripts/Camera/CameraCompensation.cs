d2bf8e8fe1c7255c4c2d9401470337a96978b51b
A.Liggins U1760724 <U1760724@unimail.hud.ac.uk>
2019-11-28 11:33:07 +0000

    Revert "merge" Reverted Mistake This reverts commit a09c4b1f6aa96e40f4f4db7d0740cdf09cb83007, reversing changes made to 37f09059e7829edecdd0d2a4f181203de2a04c2e.

--- Assets/Scripts/Camera/CameraCompensation.cs
+++ Assets/Scripts/Camera/CameraCompensation.cs
@@ -1,49 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CameraCompensation : MonoBehaviour
 {
-    //Object to compensate
-    public Transform cameraObj;
-
-    public float cameraRayDistance = 5.0f;
-    public float leftRightRayDistance = 1.0f;
-
-    private float compensationDist;
-
-    /**Fire raycast behind tracked object, if hit is less than wanted distance,
-     * translate object along object's forward vector until this is no longer true
-     */ 
-    void FixedUpdate()
+    // Start is called before the first frame update
+    void Start()
     {
-        //Backwards object tracking
-        if (Physics.Raycast(cameraObj.position, -1 * cameraObj.forward, out RaycastHit hit, cameraRayDistance))
-        {
-            if (hit.distance < cameraRayDistance)
-            {
-                compensationDist = cameraRayDistance - hit.distance;
-                cameraObj.Translate(transform.forward * compensationDist * Time.fixedDeltaTime, Space.Self);
-            }
-        }
         
-        //Right side object tracking
-        if (Physics.Raycast(cameraObj.position, cameraObj.right, out RaycastHit rightHit, leftRightRayDistance / 2.0f))
-        {
-            if ((rightHit.distance / 2.0f) < leftRightRayDistance)
-            {
-                compensationDist = leftRightRayDistance - rightHit.distance;
-                cameraObj.Translate((transform.right) * compensationDist * Time.fixedDeltaTime, Space.Self);
-            }
-        }
+    }
 
-        //Left side object tracking
-        if (Physics.Raycast(cameraObj.position, -1 * cameraObj.right, out RaycastHit leftHit, cameraRayDistance / 2.0f))
-        {
-            if ((leftHit.distance / 2.0f) < cameraRayDistance)
-            {
-                compensationDist = cameraRayDistance - leftHit.distance;
-                cameraObj.Translate(transform.right * compensationDist * Time.fixedDeltaTime, Space.Self);
-            }
-        }
+    // Update is called once per frame
+    void Update()
+    {
         
     }
 }
