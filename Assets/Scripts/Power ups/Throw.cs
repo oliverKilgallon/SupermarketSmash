7cd88ab12e68cda6e0a27e6dce44bd5fec79518f
Aliggins <u1760724@unimail.hud.ac.uk>
2020-01-14 09:32:44 +0000

    Thrown objects spin, have rigidbodies and are bean tins(for now)

--- Assets/Scripts/Power ups/Throw.cs
+++ Assets/Scripts/Power ups/Throw.cs
@@ -7,16 +7,19 @@ public class Throw : MonoBehaviour
     public Vector3[] localArc;
     public int arcStep =0;
     public float step;
+    public GameObject itemChild;
+    Vector3 rand;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rand = new Vector3(Random.Range(5, 20), Random.Range(5, 20), Random.Range(5, 20));
     }
 
     // Update is called once per frame
     void Update()
     {if (arcStep < 15)
         {
+            itemChild.transform.Rotate(rand);
             transform.position = Vector3.MoveTowards(transform.position, localArc[arcStep], step);
             if (transform.position == localArc[arcStep])
             {
