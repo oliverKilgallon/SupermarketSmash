137714c2ca9a2187e9a57f1e2a2f752a6a13b41c
A.Liggins U1760724 <U1760724@unimail.hud.ac.uk>
2020-02-06 11:53:51 +0000

    merged movemen and tweaked various things

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -12,7 +12,7 @@ public class CameraFollow : MonoBehaviour
     private Vector3 targetFollowPosition;
 
     // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
         targetFollowPosition = 
             targetFollowTransform.position + (targetFollowTransform.rotation * cameraOffsetPos);
