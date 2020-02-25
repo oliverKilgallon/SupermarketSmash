d0f2dcba6659fc51ac3db8e38124437b52fdb472
Aliggins <u1760724@unimail.hud.ac.uk>
2020-02-25 01:10:32 +0000

    fixed jam, took way too long

--- Assets/Scripts/Powerups/LifeSpan.cs
+++ Assets/Scripts/Powerups/LifeSpan.cs
@@ -6,9 +6,12 @@ public class LifeSpan : MonoBehaviour
 {
     public float timer;
     public int life;
+    public bool emit;
+    public GameObject parent;
     // Start is called before the first frame update
     void Start()
     {
+        emit = true;
         timer = 0;
     }
 
@@ -18,8 +21,8 @@ public class LifeSpan : MonoBehaviour
         timer += Time.deltaTime;
         if (timer >= life) {
             if (this.GetComponent<BoxCollider>()) { this.GetComponent<BoxCollider>().enabled = false; }
-            gameObject.transform.localScale += new Vector3(-0.0001f, 0,-0.0001f);
-            if ((gameObject.transform.lossyScale.x <= 0.06174203) && (gameObject.transform.lossyScale.z <= 0.06174203)) { Destroy(this.gameObject); }
+            gameObject.transform.localScale += new Vector3(-0.07f,-0.07f,0);
+            if ((gameObject.transform.localScale.x <= 0) && (gameObject.transform.localScale.y <= 0)) { Destroy(parent); }
         }
         
     }
--- Assets/Scripts/Powerups/Throw.cs
+++ Assets/Scripts/Powerups/Throw.cs
@@ -9,12 +9,13 @@ public class Throw : MonoBehaviour
     public float step;
     public GameObject itemChild;
     Vector3 rand;
-    bool emit;
+    public bool emit;
     public GameObject[] Emiters;
     public GameObject[] JamEmiters;
     public string weapon;
     public GameObject jam;
     public float floorY;
+    public bool reverse;
    
 
     // Start is called before the first frame update
@@ -22,6 +23,7 @@ public class Throw : MonoBehaviour
     {
         //localArc = new Vector3[16];
         emit = false;
+        reverse = false;
         rand = new Vector3(Random.Range(2, 5), Random.Range(2, 5), Random.Range(2, 5));
     }
 
@@ -34,7 +36,7 @@ public class Throw : MonoBehaviour
             {
                 itemChild.transform.Rotate(rand);
             }
-           
+
             transform.position = Vector3.MoveTowards(transform.position, localArc[arcStep], step);
             if (transform.position == localArc[arcStep])
             {
@@ -43,7 +45,7 @@ public class Throw : MonoBehaviour
             if (itemChild)
             {
                 if (arcStep == 14) { itemChild.GetComponent<Rigidbody>().useGravity = true; }
-               
+
             }
         }
         else
@@ -53,18 +55,18 @@ public class Throw : MonoBehaviour
                 itemChild.GetComponent<Rigidbody>().useGravity = true;
                 itemChild.GetComponent<BoxCollider>().enabled = true;
             }
-            
+
             //Destroy(itemChild);
-            if (emit == false&&weapon =="flour")
+            if (emit == false && weapon == "flour")
             {
-                
-                foreach(GameObject emit in Emiters)
+
+                foreach (GameObject emit in Emiters)
                 {
                     emit.GetComponent<ParticleSystem>().Play();
                 }
-                
+
                 emit = true;
-                
+
             }
             if (emit == false && weapon == "jam")
             {
@@ -75,22 +77,24 @@ public class Throw : MonoBehaviour
                 jam.gameObject.transform.SetPositionAndRotation(new Vector3(jam.gameObject.transform.position.x, floorY + 0.001f, jam.gameObject.transform.position.z), Quaternion.Euler(new Vector3(-90, 0, 0)));
                 jam.GetComponent<MeshRenderer>().enabled = true;
                 jam.GetComponent<BoxCollider>().enabled = true;
-                
+
 
                 emit = true;
- 
+
 
             }
         }
-        if((jam.transform.lossyScale.x<= 2)&& (jam.transform.lossyScale.z <= 2)&&jam.GetComponent<MeshRenderer>().enabled == true)
+        if ((jam.transform.lossyScale.x <= 2) && (jam.transform.lossyScale.z <= 2)&&!reverse)
         {
-            
-            if (emit)
+
+            if (emit && jam.GetComponent<MeshRenderer>().enabled == true)
             {
                 jam.transform.localScale += new Vector3(0.07f, 0.07f, 0);
-               // jam.transform.lossyScale.Scale(new Vector3(2, 0, 2));
+                // jam.transform.lossyScale.Scale(new Vector3(2, 0, 2));
             }
         }
+        else { reverse = true; jam.GetComponent<LifeSpan>().emit = false;jam.GetComponent<LifeSpan>().parent = this.gameObject; }
+        
     }
-    
+    // if ((jam.transform.lossyScale.x > 2) && (jam.transform.lossyScale.z > 2) && jam.GetComponent<MeshRenderer>().enabled == true){ jam.GetComponent<LifeSpan>().emit = false; emit = false; }
 }
