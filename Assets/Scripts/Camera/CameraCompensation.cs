b8a93d2ce6205d118066a838544769d372434365
oliver <u1654523@unimail.hud.ac.uk>
2020-02-27 14:05:46 +0000

    Added in wall avoidance camera compensation to check for objects behind the camera. Added in fixed loading screen animation on ready up screen.

--- Assets/Scripts/Camera/CameraCompensation.cs
+++ Assets/Scripts/Camera/CameraCompensation.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class CameraCompensation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform cameraObj;
+    public float cameraRayDistance = 3.0f;
+    Vector3 originalPos;
+    float compensationDist = 0f;
+
+    private void Start()
     {
-        
+        originalPos = cameraObj.localPosition;
+        Debug.Log(originalPos);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (Physics.Raycast(cameraObj.position, -1 * cameraObj.forward, out RaycastHit hit, cameraRayDistance))
+        {
+            if (hit.distance < cameraRayDistance)
+            {
+                compensationDist = cameraRayDistance - hit.distance;
+                cameraObj.Translate(transform.forward * compensationDist * Time.fixedDeltaTime, Space.Self);
+            }
+        }
+        else if(cameraObj.localPosition != originalPos)
+        {
+            cameraObj.Translate((originalPos - cameraObj.localPosition) * Time.fixedDeltaTime, Space.Self);
+        }
         
     }
 }
