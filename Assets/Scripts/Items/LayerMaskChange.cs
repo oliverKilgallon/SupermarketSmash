a93f844360834ee464b8b46c55e8426c1668c1ef
oliver <u1654523@HA217-03>
2020-03-04 13:12:37 +0000

    Started creating a layer mask switcher that will mean each player only sees an item highlight for items they are meant to collect. Altered product prefab so the item highlight layer is set on spawn

--- Assets/Scripts/Items/LayerMaskChange.cs
+++ Assets/Scripts/Items/LayerMaskChange.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class LayerMaskChange : MonoBehaviour
 {
+    public Camera playerCamera;
+    private int origCullingMask;
+    private string[] origItemList;
+    private List<string> localItems;
+
+    private void Awake()
+    {
+        string line;
+        int index = 1;
+        System.IO.StreamReader file =
+           new System.IO.StreamReader("Assets/Text Files/ItemStorage.txt");
+        while ((line = file.ReadLine()) != null)
+        {
+            origItemList[index] = line;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        origCullingMask = playerCamera.cullingMask;
+        localItems = GetComponent<Playerscript>().localItems;
+        for (int i = 0; i < origItemList.Length; i++)
+        {
+            for (int j = 0; j < localItems.Count; j++)
+            {
+                if (localItems[j].Equals(origItemList[i]))
+                {
+                    ShowLayer(origItemList[i]);
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleLayer(string layerName)
     {
-        
+        playerCamera.cullingMask ^= 1 << LayerMask.NameToLayer(layerName);
+    }
+
+    public void HideLayer(string layerName)
+    {
+        if (playerCamera.cullingMask == (playerCamera.cullingMask | LayerMask.NameToLayer(layerName)))
+        {
+            playerCamera.cullingMask ^= ~(1 << LayerMask.NameToLayer(layerName));
+        }
+    }
+
+    public void ShowLayer(string layerName)
+    {
+        if (!(playerCamera.cullingMask == (playerCamera.cullingMask | LayerMask.NameToLayer(layerName))))
+        {
+            playerCamera.cullingMask |= 1 << LayerMask.NameToLayer(layerName);
+        }
+    }
+
+    public void UpdateLayerMask(string[] itemList)
+    {
+        for (int i = 0; i < itemList.Length; i++)
+        {
+            if (!itemList[i].Equals(""))
+            {
+                HideLayer(itemList[i]);
+            }
+            else
+            {
+                ShowLayer(itemList[i]);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Items/SetProductLayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SetProductLayer : MonoBehaviour
+{
+    public GameObject itemHighlight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string layerName = GetComponent<ItemScript>().product;
+        itemHighlight.layer = LayerMask.NameToLayer(layerName);
+    }
+}
